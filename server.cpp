2bc84d14197eb912691b9dba08002cc3bc50046b
hvu <hoang040507@gmail.com>
2026-04-24 00:29:44 -0500

    Close the socket

--- server.cpp
+++ server.cpp
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 int main() {
    printf("sub bro\n");
@@ -16,18 +17,39 @@ int main() {
    else
       printf("Error: Invalid IP address format.\n");
 
-   if (bind(socketfd, (sockaddr *)(&addr), sizeof(addr)) != 0)
+   if (bind(socketfd, (sockaddr *)(&addr), sizeof(addr)) == -1)
       printf("Error: Failed to bind address to socket\n");
    else
       printf("Successfully bound address to socket\n");
 
    constexpr int CONNECTION_LIMIT = 5;
-   if (listen(socketfd, CONNECTION_LIMIT) != 0)
+   if (listen(socketfd, CONNECTION_LIMIT) == -1)
       printf("Error: Failed to listen");
    else
       printf("Listening...");
 
-   // accept();
+   int sin_size = sizeof(struct sockaddr_in);
+   int connected_socketfd;  // socket fd to communicate with a client
+   sockaddr_in client_addr; // ip addr of the client
+
+   connected_socketfd = accept(socketfd, (sockaddr *)&client_addr, (socklen_t *)&sin_size);
+   if (connected_socketfd == -1)
+      printf("Error: Failed to accept connection");
+   else
+      printf("Listening...");
+
+   constexpr int max_msg_len = 100;
+   char incoming_msg[max_msg_len];
+
+   int msg_signal = recv(connected_socketfd, incoming_msg, max_msg_len, 0);
+   if (msg_signal == -1) {
+      printf("Error: Failed to receive msg");
+   } else if (msg_signal == 0) {
+      printf("Client closed connection");
+   } else
+      printf("Msg received");
+
+   close(socketfd); // delete the socket descriptor, disallowing any further connection.
 
    return 0;
 }
